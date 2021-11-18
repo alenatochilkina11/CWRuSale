8aa3e1c16377eedcf10d8dc3cb3f10e678fe7434
Alena Tochilkina <alenatochilkina@alenas-air.home>
2021-11-18 01:30:47 -0500

    connected request form to request-item-db

--- my-app/src/Page/ItemRequest.js
+++ my-app/src/Page/ItemRequest.js
@@ -1,9 +1,17 @@
 import ItemRequestForm from "../Components/Requests/ItemRequestForm";
 function ItemsRequestPage() {
+  const requestItemHandler = (itemData) => {
+    fetch(
+      "https://cwru-sale.azurewebsites.net/api/request-item?" +
+        new URLSearchParams(itemData),
+      { method: "GET" }
+    );
+  };
+
   return (
     <section>
       <h1>Be Notified of Item Uploads</h1>
-      <ItemRequestForm />
+      <ItemRequestForm onRequestItem={requestItemHandler} />
     </section>
   );
 }
