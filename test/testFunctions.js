fe6e14269346de64b93cc17df6813e60633a8735
Amy Churchill <73766423+happyamy19@users.noreply.github.com>
2021-11-17 19:42:33 -0500

    removed testing

--- test/testFunctions.js
+++
@@ -1,8 +0,0 @@
-const assert = require('chai').assert;
-const testing = require('../testing');
-
-describe('testing', function(){
-    it('return hello', function(){
-        assert.equal(testing(), 'hello');
-    });
-});
--- testing.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(){
-    // testSearchItems: function(){
-    //     return "hello";
-    // }
-    return "hello";
-}
