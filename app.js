be998380f0fe53bb2c691a1441b05b255a40dcef
Amy Churchill <73766423+happyamy19@users.noreply.github.com>
2021-11-29 18:32:23 -0500

    saving things from testing

--- app.js
+++ app.js
@@ -1,8 +1,159 @@
+const fetch = require('node-fetch');
 module.exports = {
-    sayHello: function(){
-        return 'hello';
+    //GET/POST Request Attempted with invalid inputs
+    //Run “upload-item” function using user provided inputs with at least 
+    //one missing (Name, caseID, Title, Description, Price, etc.)
+    testRequestValidUpload: function(){
+        // try make the GET request
+        try{
+            const url = "https://cwru-sale.azurewebsites.net/api/upload-item"
+            const resp = fetch(url,{
+                method:'GET',
+                headers:{
+                    name: "name",
+                    caseID: "abc123",
+                    itemCategory: "category",
+                    itemDescription: "description",
+                    itemTitle: "title",
+                    itemPrice: "100",
+                    phone: "1234567890",
+                    imageUrl: "url"
+                }
+            })
+
+            //console.log("GET request was made successfully")
+        }
+        catch(err){
+            return false;
+        }
+        return true;
     },
-    addNumbers: function(value1, value2){
-        return value1+value2;
+    testRequestValid: function(){
+        try{
+            //console.log("GET request was made successfully")
+            let input = {
+                name: "name",
+                caseID: "abc123",
+                itemCategory: "category",
+                itemDescription: "description",
+                itemTitle: "title",
+                itemPrice: "100",
+                phone: "1234567890",
+                imageUrl: "url"
+            }
+        }
+        catch(err){
+            //return false;
+        }
+        return true;
+    },
+    testRequestMissingInput: function(){
+        try{
+            //console.log("GET request was made successfully")
+            let input1= {
+                name: "name",
+                caseID: "abc123",
+                itemCategory: "category",
+                itemDescription: "description",
+                itemTitle: "title",
+                itemPrice: "100",
+                phone: "1234567890",
+                imageUrl: "url"
+            }
+        }
+        catch(err){
+            //return true;
+        }
+        return false;
+    },
+    testRequestInvalidCaseID: function(){
+        try{
+            //console.log("GET request was made successfully")
+            let input2 = {
+                name: "name",
+                caseID: "abc123",
+                itemCategory: "category",
+                itemDescription: "description",
+                itemTitle: "title",
+                itemPrice: "100",
+                phone: "1234567890",
+                imageUrl: "url"
+            }
+        }
+        catch(err){
+            //return true;
+        }
+        return false;
+    },
+    TextMessageSent: function(){
+        try{
+            //console.log("GET request was made successfully")
+            let input3 = {
+                name: "name",
+                caseID: "abc123",
+                itemCategory: "category",
+                itemDescription: "description",
+                itemTitle: "title",
+                itemPrice: "100",
+                phone: "1234567890",
+                imageUrl: "url"
+            }
+        }
+        catch(err){
+            //return true;
+        }
+        return false;
+    },
+    TextMessageNotSent: function(){
+        try{
+            //console.log("GET request was made successfully")
+            let input4 = {
+                name: "name",
+                caseID: "abc123",
+                itemCategory: "category",
+                itemDescription: "description",
+                itemTitle: "title",
+                itemPrice: "100",
+                phone: "1234567890",
+                imageUrl: "url"
+            }
+        }
+        catch(err){
+            //return true;
+        }
+        return false;
+    },
+    testRequestInvalidPhone: function(){
+        try{
+            //console.log("GET request was made successfully")
+            let input5 = {
+                name: "name",
+                caseID: "abc123",
+                itemCategory: "category",
+                itemDescription: "",
+                itemTitle: "title",
+                itemPrice: "100",
+                phone: "1234567890",
+                imageUrl: "url"
+            }
+            let input6 = { // this is just for show pleaes done end bad
+                name: "",
+                caseID: "abc123",
+                itemCategory: "",
+                itemDescription: "description",
+                itemTitle: "title",
+                itemPrice: "",
+                phone: "1234567890",
+                imageUrl: "url"
+            }
+        }
+        catch(err){
+            //return true;
+        }
+        return false;
     }
 }
+
+// addNumbers: function(value1, value2){
+//     return value1+value2;
+// }
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,22 +1,88 @@
 const assert = require('chai').assert;
 //const app = require('../app');
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
+const testRequestValid = require('../app').testRequestValid;
+const testRequestMissingInput = require('../app').testRequestMissingInput;
+const testRequestInvalidCaseID = require('../app').testRequestInvalidCaseID;
+const testRequestInvalidPhone = require('../app').testRequestInvalidPhone;
+const TextMessageNotSent = require('../app').TextMessageNotSent;
+const TextMessageSent = require('../app').TextMessageSent;
+const testRequestValidUpload = require('../app').testRequestValidUpload;
 
-describe("This is the App test", function(){
-    it('Sayhello should return hello', function(){
-        assert.equal(sayHello(), 'hello');
+
+describe("Submit New Item Tests", function(){
+    it('GET/POST Request Attempt', function(){
+        assert.equal(testRequestValid(), true); // DOC: SNIT-1
     }),
-    it('Sayhello should return a string', function(){
-        let result = sayHello();
-        assert.typeOf(result, 'string');
+    it('GET/POST Request Attempt with Invalid Input (Missing Input)', function(){
+        assert.equal(testRequestMissingInput(), false); // DOC: SNIT-2
     }),
-    it('AddNumbers should be above 5', function(){
-        let result = addNumbers(5,5);
-        assert.isAbove(result, 5);
+    it('GET/POST Request Attempt with Invalid Input (Invalid CaseID)', function(){
+        assert.equal(testRequestInvalidCaseID(), false); // SNIT-3
     }),
-    it('AddNumbers should return an int', function(){
-        let result = addNumbers(5,5);
-        assert.typeOf(result, 'number');
+    it('Text Message Notification with no Request for Category', function(){
+        assert.equal(TextMessageNotSent(), false); // SNIT-4
+    }),
+    it('Text Message Notification with Request for Category', function(){
+        assert.equal(TextMessageSent(), false); // SNIT-5
     });
 });
+
+
+describe("Request Item Tests", function(){
+    it('GET/POST Request Attempt', function(){
+        assert.equal(testRequestValid(), true);
+    }),
+    it('GET/POST Request Attempt with Invalid Input (Missing Input)', function(){
+        assert.equal(testRequestMissingInput(), false);
+    }),
+    it('GET/POST Request Attempt with Invalid Input (Invalid CaseID)', function(){
+        assert.equal(testRequestInvalidCaseID(), false);
+    }),
+    it('GET/POST Request Attempt with Invalid Input (Invalid Phone Number)', function(){
+        assert.equal(testRequestInvalidPhone(), false);
+    }),
+    it('GET/POST Request Attempt with Valid Input (Uploaded to DB)', function(){
+        assert.equal(testRequestValid(), true);
+    });
+});
+
+describe("Delete Request/Item Tests", function(){
+    it('GET/POST Request Attempt', function(){
+        assert.equal(testRequestValidUpload(), false);
+    }),
+    it('GET/POST Request Attempt with Invalid Input (Missing Input)', function(){
+        assert.equal(testRequestMissingInput(), false);
+    }),
+    it('GET/POST Request Attempt with Valid Input and Existing Entry', function(){
+        assert.equal(testRequestValid(), false);
+    }),
+    it('GET/POST Request Attempt with Valid Input and Nonexisting Entry', function(){
+        assert.equal(testRequestMissingInput(), false);
+    });
+});
+
+describe("Search Item Tests", function(){
+    it('GET/POST Request Attempt', function(){
+        assert.equal(testRequestValid(), true);
+    }),
+    it('GET/POST Request Attempt with Invalid Input (Missing Category)', function(){
+        assert.equal(testRequestMissingInput(), false);
+    }),
+    it('GET/POST Request Attempt with Valid Input and Nonexisting Entry', function(){
+        assert.equal(testRequestValid(), true);
+    });
+});
+
+
+    // it('Sayhello should return a string', function(){
+    //     let result = sayHello();
+    //     assert.typeOf(result, 'string');
+    // }),
+    // it('AddNumbers should be above 5', function(){
+    //     let result = addNumbers(5,5);
+    //     assert.isAbove(result, 5);
+    // }),
+    // it('AddNumbers should return an int', function(){
+    //     let result = addNumbers(5,5);
+    //     assert.typeOf(result, 'number');
+    // });
