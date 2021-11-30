b79a7a38d262aa2086df3172a5577fcf19ef7780
Amy Churchill <73766423+happyamy19@users.noreply.github.com>
2021-11-30 17:56:51 -0500

    fixed management and updated upload item to find matching requests in database by category

--- management/index.js
+++ management/index.js
@@ -1,7 +1,15 @@
 
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 
-  const config = {
+  const config_items = {
+    endpoint: process.env.COSMOS_ENDPOINT,
+    key: process.env.COSMOS_KEY,
+    databaseId: "cwru-sale",
+    containerId: "uploaded-items",
+    partitionKey: {kind: "Hash", paths: ["/items"]}
+  }; 
+
+  const config_requests = {
     endpoint: process.env.COSMOS_ENDPOINT,
     key: process.env.COSMOS_KEY,
     databaseId: "cwru-sale",
@@ -12,9 +20,9 @@ const CosmosClient = require("@azure/cosmos").CosmosClient;
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    let requestID = req.query.requestID
+    let id = req.query.id
 
-    let responseMessage = await findItemsToDelete(requestID)
+    let responseMessage = await findDataToDelete(id)
 
     context.res = {
         // status: 200, /* Defaults to 200 */
@@ -23,50 +31,70 @@ module.exports = async function (context, req) {
 }
 
 
-async function findItemsToDelete(idToDelete){
+async function findDataToDelete(idToDelete){
+      let message; // message to be returned to user
+      let category;
 
-    const { endpoint, key, databaseId, containerId } = config;
+      let [items, item_container] = await checkItemsWithID(idToDelete)
+      let [requests, requests_container] = await checkRequestsWithID(idToDelete)
+
+      if (items.length>0){
+        category = "item"
+        container = item_container
+      }
+      else if (requests.length>0){
+        category = "request"
+        container = requests_container
+      }
+      else{
+        message = "Request/Item with ID does not exist."
+        return message
+      }
+
+      try{
+          const { resource: result } = await container.item(idToDelete, category).delete();
+          message = "Request/Item deleted successfully."
+      }
+      catch(err){
+          message = "Request/Item with ID Exists, failed to delete."
+        }
+    return message;
+  }
+
+  async function checkRequestsWithID(id){
+    const { endpoint, key, databaseId, containerId } = config_requests;
     const client = new CosmosClient({ endpoint, key });
     const database = client.database(databaseId);
     const container = database.container(containerId);
 
-      // query to return all items
-      const querySpec = {
-          query: `SELECT * FROM c WHERE c.id = "${idToDelete}"`
-      };
+    // query to return all requests with matching category
+    const querySpec = {
+      query: `SELECT * FROM c WHERE c.id = "${id}"`
+    };
 
-      // read all items in the Items container
-      const { resources: items } = await container.items
-          .query(querySpec)
-          .fetchAll();
-      
-      let message;
-
-      let exists = false;
-      for (let i = 0; i < items.length; i++) {
-          if (items[i].id == idToDelete){
-            exists = true;
-            break;
-          }
-      }
-
-      if (exists){
-        try{
-          const { resource: result } = await container.item(id).delete();
-          message = "Request Deleted"
-        }
-        catch(err){
-          message = "Request with ID Exists, Failed to Delete"
-        }
-      }
-      else{
-        message = "Request with ID Does not Exist"
-      }
-
-     // category may need to change to "<partition-key-value" // const { resource: result } = await container.item(id, category).delete();
-    return message;
+    // read all items in the Requests in container, store in items
+    const { resources: requests } = await container.items
+        .query(querySpec)
+        .fetchAll();
+    // returns the array of requests with all information
+    return [requests, container];
   }
 
-  // add category option
-  // insert something into the requests
-  // try delete using that category
+  async function checkItemsWithID(id){
+    const { endpoint, key, databaseId, containerId } = config_items;
+    const client = new CosmosClient({ endpoint, key });
+    const database = client.database(databaseId);
+    const container = database.container(containerId);
+
+    // query to return all requests with matching category
+    const querySpec = {
+      query: `SELECT * FROM c WHERE c.id = "${id}"`
+    };
+
+    // read all items in the Requests in container, store in items
+    const { resources: items } = await container.items
+        .query(querySpec)
+        .fetchAll();
+    // returns the array of requests with all information
+    return [items, container];
+  }
--- upload-item/index.js
+++ upload-item/index.js
@@ -1,6 +1,6 @@
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 
-const config = {
+const config_items = {
     endpoint: process.env.COSMOS_ENDPOINT,
     key: process.env.COSMOS_KEY,
     databaseId: "cwru-sale",
@@ -8,10 +8,17 @@ const config = {
     partitionKey: {kind: "Hash", paths: ["/items"]}
   }; 
 
+  const config_requests = {
+    endpoint: process.env.COSMOS_ENDPOINT,
+    key: process.env.COSMOS_KEY,
+    databaseId: "cwru-sale",
+    containerId: "requested-items",
+    partitionKey: {kind: "Hash", paths: ["/requests"]}
+  }; 
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    //const name = (req.query.name || (req.body && req.body.name));
     let name = req.query.name
     let caseID = req.query.caseID
     let itemCategory = req.query.itemCategory
@@ -20,6 +27,7 @@ module.exports = async function (context, req) {
     let itemPrice = req.query.itemPrice
     let phone = req.query.phone
     let imageUrl = req.query.imageUrl
+
 
     //let newItemInfo = [name, caseID, itemCategory, itemDescripton, itemTitle, itemPrice, phone, imageUrl]
 
@@ -33,12 +41,16 @@ module.exports = async function (context, req) {
         itemTitle: itemTitle,
         itemPrice: itemPrice,
         phone: phone,
-        imageUrl: imageUrl
+        imageUrl: imageUrl,
+        items: "item"
         }
+
+    let requests = await getRequestsWithCategory(itemCategory);
+    // next steps: send text message to all requests phone numbers in the array that a new item has been added to their category!
 
     let entries = await createDocument(newItemEntry);
     
-    const responseMessage = `Thank you, ${entries[entries.length-1].name}. Your item id is: ${entries[entries.length-1].id}`
+    const responseMessage = `Thank you, ${entries[entries.length-1].name}. Your item id is: ${entries[entries.length-1].id}. Number of Requests with matching category: ${requests.length}`
     
     context.res = {
         // status: 200, /* Defaults to 200 */
@@ -47,7 +59,7 @@ module.exports = async function (context, req) {
 }
 
 async function create(client, databaseId, containerId) {
-    const partitionKey = config.partitionKey;
+    const partitionKey = config_items.partitionKey;
   
     /**
      * Create the database if it does not exist
@@ -71,7 +83,7 @@ async function create(client, databaseId, containerId) {
   }
 
   async function createDocument(newItem){
-    const { endpoint, key, databaseId, containerId } = config;
+    const { endpoint, key, databaseId, containerId } = config_items;
 
     const client = new CosmosClient({ endpoint, key });
     
@@ -92,6 +104,25 @@ async function create(client, databaseId, containerId) {
         .query(querySpec)
         .fetchAll();
     
-    
     return items;
+  }
+
+// this function will take in the new category and search the requests database and retreieve an array of all the requests with the matched category. 
+  async function getRequestsWithCategory(category){
+    const { endpoint, key, databaseId, containerId } = config_requests;
+    const client = new CosmosClient({ endpoint, key });
+    const database = client.database(databaseId);
+    const container = database.container(containerId);
+
+    // query to return all requests with matching category
+    const querySpec = {
+        query: `SELECT * from c WHERE c.categoryRequested = "${category}"`
+    };
+
+    // read all items in the Requests in container, store in items
+    const { resources: requests } = await container.items
+        .query(querySpec)
+        .fetchAll();
+    // returns the array of requests with all information
+    return requests;
   }
